485c33f298a0a7b20c53d84a1fb0e170d48f2204
jepoyramos <jeffrey.ramos@make.technology>
2022-09-21 11:38:22 +0800

    feat: MindArc Exercise 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,7 @@
-// Import your JS components here
-import "./lazy-loading";
-import { Sample } from "./components/sample";
-
-class App {
-  constructor() {
-    this.components = [];
-  }
-
-  component(component) {
-    this.components.push(component);
-  }
-
-  boot() {
-    this.components.forEach((component) => component.init());
-  }
-}
-
-const app = new App();
-// Push imported JS to component variable using .component method
-app.component(new Sample());
-
-document.addEventListener("DOMContentLoaded", () => app.boot());
-
-
 /**
  * Initialize Loading State
  */
-// const loadingState = new LoadingState();
-// window.addEventListener('load', (event) => loadingState.set('success'));
+ window.addEventListener('load', (event) => {
+  document.querySelector('main').classList.remove('loading');
+});
 
